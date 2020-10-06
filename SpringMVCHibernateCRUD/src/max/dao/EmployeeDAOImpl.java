ab61cfedb058ce89b57b0b73b13b7b589ad93c3a
NASIR <NASIR@LAPTOP-RJBL907I>
2020-10-06 15:28:22 +0530

    firt proj added

---
+++ SpringMVCHibernateCRUD/src/max/dao/EmployeeDAOImpl.java
@@ -0,0 +1,54 @@
+package max.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import max.model.Employee;
+
+
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void addEmployee(Employee employee) {
+		//openSession->it always creates a new Session object and give it to you
+		//getCurrentSession->create a new session if does not exist
+		sessionFactory.getCurrentSession().saveOrUpdate(employee);
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Employee> getAllEmployees() {
+
+		return sessionFactory.getCurrentSession().createQuery("from Employee")
+				.list();
+	}
+
+	@Override
+	public void deleteEmployee(Integer employeeId) {
+		Employee employee = (Employee) sessionFactory.getCurrentSession().load(
+				Employee.class, employeeId);
+		if (null != employee) {
+			this.sessionFactory.getCurrentSession().delete(employee);
+		}
+
+	}
+
+	public Employee getEmployee(int empid) {
+		return (Employee) sessionFactory.getCurrentSession().get(
+				Employee.class, empid);
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		sessionFactory.getCurrentSession().update(employee);
+		return employee;
+	}
+
+}
---
+++ SpringMVCHibernateCRUD/src/max/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package max.service;
+
+import java.util.List;
+
+import max.model.Employee;
+
+public interface EmployeeService {
+	
+	public void addEmployee(Employee employee);
+
+	public List<Employee> getAllEmployees();
+
+	public void deleteEmployee(Integer employeeId);
+
+	public Employee getEmployee(int employeeid);
+
+	public Employee updateEmployee(Employee employee);
+}
+/*
+ * public void addEmployee(Employee employee);
+ * 
+ * public List<Employee> getAllEmployees();
+ * 
+ * public void deleteEmployee(Integer employeeId);
+ * 
+ * public Employee updateEmployee(Employee employee);
+ * 
+ * public Employee getEmployee(int employeeid);
+ */
